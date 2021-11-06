327d9904aaabfb9bc3b453f576cd1f9e0da53dfe
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-11-06 10:00:50 -0500

    Fix debug mode for local

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,7 @@ import * as fs from "fs";
 import * as util from "util";
 
 const readFile = util.promisify(fs.readFile);
-
-const { DEBUG } = process.env;
+const { VSCODE_DEBUG = false } = process.env;
 
 export async function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand("extension.tree.reload", () => {
@@ -19,9 +18,8 @@ export async function activate(context: vscode.ExtensionContext) {
   const provider = vscode.languages.registerFoldingRangeProvider({ language: "tree" }, foldingProvider);
   context.subscriptions.push(provider);
 
-  
-  if (DEBUG) {
-    // open new file with tree lang file when VSCODE_DEBUG_MODE is set
+  // open new file with tree lang file when VSCODE_DEBUG is set
+  if (VSCODE_DEBUG) {
     const fileUrl = path.join(context.extensionPath, "example.tree");
     const exampleFile = await readFile(fileUrl.toString());
     const document = await vscode.workspace.openTextDocument({
---
+++ test.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-undef
+process.env;
+
+// eslint-disable-next-line no-undef
+console.log(1)
