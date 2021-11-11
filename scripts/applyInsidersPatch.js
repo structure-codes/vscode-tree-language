255476e09e5f448f151ad308ea93c84ff8eac38b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-11-10 21:09:01 -0600

    Fix insiders CD

--- scripts/applyInsidersPatch.js
+++ scripts/applyInsidersPatch.js
@@ -5,14 +5,19 @@ const fs = require("fs");
 const date = new Date(new Date().toLocaleString("en-US", { timeZone: "America/New_York" }));
 let json = require("../package.json");
 
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+const hour = date.getHours();
+
+const jobId = process.env.GITHUB_JOB;
+const version = `${year}.${month}.${day}${hour}-${jobId}`;
+
 json = JSON.stringify({
   ...json,
   name: `${json.name}-insiders`,
   displayName: "Tree (Insiders)",
-  version: `${String(date.getFullYear())}.${date.getMonth() + 1}.${date.getDate()}${String(date.getHours()).padStart(
-    2,
-    "0"
-  )}`,
+  version,
   preview: true,
 });
 fs.writeFileSync("./package.json", json);
