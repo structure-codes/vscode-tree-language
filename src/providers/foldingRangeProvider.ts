0e05996d1349ad56a47070534138c420136d6e3b
Alex Oser <aoser9552@gmail.com>
2022-03-06 14:58:36 -0600

    Bump utils version

--- src/providers/foldingRangeProvider.ts
+++ src/providers/foldingRangeProvider.ts
@@ -5,7 +5,7 @@ const getLastNode = (branch: TreeType) => {
   const indexes: Array<number> = [];
   // Get the last line number that is a child of the given branch
   const getIndexes = (branch: TreeType) => {
-    if (branch.children.length === 0) indexes.push(branch.index);
+    if (branch.children.length === 0) indexes.push(branch._index);
     branch.children.forEach((child) => {
       getIndexes(child);
     });
@@ -20,7 +20,7 @@ export function provideFoldingRanges(document: vscode.TextDocument): vscode.Prov
   if (tree.length === 0) return ranges;
 
   const getRanges = (branch: TreeType) => {
-    const startIndex = branch.index;
+    const startIndex = branch._index;
     const endIndex = getLastNode(branch);
     ranges.push(new vscode.FoldingRange(startIndex, endIndex, vscode.FoldingRangeKind.Region));
     branch.children.forEach((leaf) => {
