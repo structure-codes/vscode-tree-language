0f37d048000dcacca3ec0a7416563a4c37c27dbd
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-03-13 19:02:01 -0500

    Hack the workbench.editor on and off

--- src/providers/textDocumentChangeProvider.ts
+++ src/providers/textDocumentChangeProvider.ts
@@ -2,22 +2,44 @@ import * as vscode from "vscode";
 
 const TREE_CHARS = ["├──", "└──"];
 
-// TODO: This is getting rekt by the setting: workbench.editor.languageDetection being true
-export function textDocumentChangeProvider(event: vscode.TextDocumentChangeEvent) {
+export async function textDocumentChangeProvider(event: vscode.TextDocumentChangeEvent) {
   const { isUntitled, version } = event.document;
   const isFirstPaste = isUntitled && version <= 2;
 
-  // console.log("isFirstPaste", isFirstPaste, "isUitite;d", isUntitled, "version", version);
+  // this will abort if it's not the first paste
   if (!isFirstPaste) {
     return;
   }
 
   const textDocument = event.document;
+
+  // TODO: this should be optimized to only parse the first x lines as well as only look maybe 5-10 chars over?
+  // so maybe a get range 0-50 and then grab only the first 5-10 chars to scan
   const text = textDocument.getText();
+
+  // get the editor config
+  const wsConfig = vscode.workspace.getConfiguration("workbench.editor");
+
+  // get current value
+  const autoFormatDefault = wsConfig.get("languageDetection");
 
   // test if the doc has any tree chars
   if (TREE_CHARS.some((v: string) => text.includes(v))) {
-    console.log("Containers tree char, setting lang");
-    vscode.languages.setTextDocumentLanguage(textDocument, "tree");
+    console.log("Tree language was detected, disabled autoformat then enabling syntax highlighting");
+    
+    // disable auto format
+    await wsConfig.update("languageDetection", false, vscode.ConfigurationTarget.Global);
+    console.log("Disabled autoformat...");
+    console.log("setTextDocumentLanguage start...");
+    await vscode.languages.setTextDocumentLanguage(textDocument, "tree");
+    console.log("setTextDocumentLanguage end...");
+
+    // HACK: reset to users default after 500ms
+    setTimeout(async() => { 
+      console.log("Reset autoformat start...");
+      await wsConfig.update("languageDetection", autoFormatDefault, vscode.ConfigurationTarget.Global);
+      console.log("Reset autoformat end...");
+    }, 500);     
+    
   }
 }
